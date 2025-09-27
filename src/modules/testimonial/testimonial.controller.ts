4b5d03c1240073b0856021eb919f69eea562bbec
safimiazi <mohibullamiazi@gmail.com>
2025-09-27 16:57:08 +0600

    update

--- src/modules/testimonial/testimonial.controller.ts
+++ src/modules/testimonial/testimonial.controller.ts
@@ -20,22 +20,32 @@ import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('testimonials')
 export class TestimonialController {
-  constructor(private readonly testimonialService: TestimonialService) {}
+  constructor(private readonly testimonialService: TestimonialService) { }
 
   @UseGuards(JwtAuthGuard)
   @Post('create')
-      @UseInterceptors(FileInterceptor('avatar'))
-  
-  async create(@Body() body: any, @Req() req: any,        @UploadedFile() avatar?: Express.Multer.File,
+  @UseInterceptors(FileInterceptor('avatar'))
+
+  async create(@Body() body: any, @Req() req: any, @UploadedFile() avatar?: Express.Multer.File,
   ) {
     const userId = req.user.userId;
-    const data = await this.testimonialService.create(userId, body,avatar);
+    const data = await this.testimonialService.create(userId, body, avatar);
     return successResponse(data, 'Testimonial created successfully');
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('get-all')
   async findAll(
+    @Query('page') page = 1,
+    @Query('limit') limit = 10,
+    @Query('search') search?: string,
+    @Query('order') order: 'asc' | 'desc' = 'desc',
+  ) {
+    const data = await this.testimonialService.findAll(Number(page), Number(limit), search, order);
+    return successResponse(data, 'Testimonials fetched successfully');
+  }
+  @Get('get-all-for-showcase')
+  async findAllTestimonial(
     @Query('page') page = 1,
     @Query('limit') limit = 10,
     @Query('search') search?: string,
@@ -52,19 +62,19 @@ export class TestimonialController {
     return successResponse(data, 'Testimonial fetched successfully');
   }
 
-@UseGuards(JwtAuthGuard)
-@Put(':id')
-@UseInterceptors(FileInterceptor('avatar')) // handle uploaded file
-async update(
-  @Param('id', ParseIntPipe) id: number,
-  @Body() body: any,
-  @Req() req: any,
-  @UploadedFile() avatar?: Express.Multer.File,
-) {
-  const userId = req.user.userId;
-  const data = await this.testimonialService.update(id, userId, body, avatar);
-  return successResponse(data, 'Testimonial updated successfully');
-}
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
+  @UseInterceptors(FileInterceptor('avatar')) // handle uploaded file
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: any,
+    @Req() req: any,
+    @UploadedFile() avatar?: Express.Multer.File,
+  ) {
+    const userId = req.user.userId;
+    const data = await this.testimonialService.update(id, userId, body, avatar);
+    return successResponse(data, 'Testimonial updated successfully');
+  }
 
 
   @UseGuards(JwtAuthGuard)
