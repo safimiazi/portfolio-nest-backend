30255a5d0dee640fbcd76e31e3460e6f0c0eaf90
safimiazi <mohibullamiazi@gmail.com>
2025-09-28 09:00:30 -0600

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true });
+
+  // Allow all origins
+  app.enableCors({
+    origin: '*',   // সব origin allow
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 5000);
 }
